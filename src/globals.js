6c5804f609321385619b769f76f6bb7fcfbc766c
Chris <cng177@my.bcit.ca>
2020-03-02 15:31:08 -0800

    Refactor server

--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,6 @@
 exports.port = 3000
+
+exports.map = {
+  '/': `<p>Hello</p><a href='/test'>/test</a>`,
+  '/test': `<p>Sam</p><a href='/'>Go back</a>`
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,7 @@
 const Koa = require('koa')
-const { port } = require('./globals')
+const { port, map } = require('./globals')
 const { messages } = require('./db/db')
 const server = new Koa()
-
-const map = {
-  '/': `<p>Hello</p><a href='/test'>/test</a>`,
-  '/test': `<p>Sam</p><a href='/'>Go back</a>`
-}
 
 server.use( (ctx) => ctx.body = map[ctx.path] )
 
