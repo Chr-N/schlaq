9ab2e368cfa2e3dceefae7bbded74f15b0fa4c1c
Chris <cng177@my.bcit.ca>
2020-03-24 14:52:53 -0700

    Update engineOpts + props

--- src/globals.js
+++ src/globals.js
@@ -3,12 +3,24 @@ exports.viewsDir =  `${__dirname}/views`
 
 exports.engineOpts = {
   babel: {
-    presets: [ '@babel/preset-env', '@babel/preset-react' ],
+    presets: [
+      [
+        '@babel/preset-env', {
+          targets: { esmodules: true },
+          bugfixes: true
+        }
+      ],
+      [
+        '@babel/preset-react', {
+          useSpread: true
+        }
+      ]
+    ],
     plugins: [ [ 'styled-jsx/babel', { optimizeForSpeed: true } ] ]
   }
 }
 
 exports.props = {
-  title: 'Schlack',
+  title: 'Schlaq',
   msg: 'the quick brown fox jumped over the lazy dog'
 }
