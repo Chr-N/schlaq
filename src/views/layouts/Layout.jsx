dea12e6a21da0f45f3523b675d02979d628f9101
Chris <cng177@my.bcit.ca>
2020-03-28 00:12:34 -0700

    Add favicon

--- src/views/layouts/Layout.jsx
+++ src/views/layouts/Layout.jsx
@@ -8,7 +8,8 @@ export default ({ title, children }) => {
     <html>
       <head>
         <title>{title}</title>
-        <link rel='stylesheet' href='tw/base.min.css' />
+        <link rel='stylesheet' href='tw/base.min.css'/>
+        <link rel='shortcut icon' href='favicon.svg' type='image/x-icon'/>
       </head>
       <body css={css`color:${c.fgTxt};`}>
         {children}
