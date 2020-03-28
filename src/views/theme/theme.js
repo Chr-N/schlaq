727700212682d75d68f49d6262f5af20c0099736
Chris <cng177@my.bcit.ca>
2020-03-28 13:48:17 -0700

    Refactor theme colours from hex to rgb

--- src/views/theme/theme.js
+++ src/views/theme/theme.js
@@ -1,14 +1,14 @@
 export default {
-  bg: '#1a1d21',
-  bgSidebar: '#19171d',
-  bgHeader: '#121016',
-  bgInput: '#222529',
-  txt: '#d1d2d3',
-  txtSec: '#ababad',
-  txtDim: '#919193',
-  online: '#2bac76',
-  highlight: '#1164a3',
-  link: '#1164a3',
-  outline: '#333',
-  hover: '#333'
+  bg: 'rgb(26,29,33)',
+  bgSidebar: 'rgb(25,23,29)',
+  bgHeader: 'rgb(18,16,22)',
+  bgInput: 'rgb(34,37,41)',
+  txt: 'rgb(209,210,211)',
+  txtSec: 'rgb(171,171,173)',
+  txtDim: 'rgb(145,145,147)',
+  online: 'rgb(43,172,118)',
+  highlight: 'rgb(17,100,163)',
+  link: 'rgb(17,100,163)',
+  outline: 'rgb(51,51,51)',
+  hover: 'rgb(51,51,51)'
 }
