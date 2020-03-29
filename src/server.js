2e81a49fd32c2d4a7f866d6f59880c27dd2994f3
justin <justin_yih@hotmail.com>
2020-03-28 17:01:44 -0700

    forget changes

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ server.set( 'views', viewsDir )
 server.set( 'view engine', 'jsx' )
 server.engine( 'jsx', reactViews.createEngine( engineOpts ) )
 
-server.get( '/', (req,res) => res.render( 'App', {...props, name: 'bob'} ) )
+server.get( '/', (req,res) => {
+    res.render( 'App', {...props, name: 'bob'} ) 
+})
 
 server.listen( port, () => console.log( `\nServer is live at http://localhost:${port}` ) )
