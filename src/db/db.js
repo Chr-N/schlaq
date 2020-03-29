d6e2b2a1c43dd37cb95921ae81935c5d2668fdaa
Chris <cng177@my.bcit.ca>
2020-03-28 22:18:17 -0700

    Update placeholder database

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,120 @@
-exports.messages = [
-  {
-    id: "1",
-    content: "This is a message",
-    username: "sam",
-    avatarImage: "https://cdn.discordapp.com/attachments/620095242728636416/681630720581828612/unknown.png"
-  }
+exports.starred = [
+  { starred: 'Bob' },
+  { starred: 'Tom' }
 ]
 
-exports.getMessages = (callback) => {
+exports.channels = [
+  { channel: 'general' },
+  { channel: 'random' },
+  { channel: 'questions' },
+  { channel: 'announcements' },
+  { channel: 'discussion' },
+  { channel: 'more random' }
+]
+
+exports.dms = [
+  { dm: 'Bob' },
+  { dm: 'David' },
+  { dm: 'Sarah' },
+  { dm: 'Tessa' },
+  { dm: 'Carey' },
+  { dm: 'Tim' },
+  { dm: 'Tom' },
+  { dm: 'Jake' },
+  { dm: 'Bill' },
+  { dm: 'Dave' }
+]
+
+// messages array per user
+// { [scope:string]: message[] }
+exports.messages = {
+  Bob: [
+    {
+      id: '1',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non molestie urna. Suspendisse dolor lacus, suscipit id malesuada et, congue id sem. Praesent ut sollicitudin nisl. Fusce mollis, urna ut feugiat imperdiet, odio orci mollis nunc, quis imperdiet quam urna vel erat. Nam auctor sed nulla vel venenatis. Nullam ac risus ut est scelerisque molestie. Fusce sed euismod lorem, sed tempus neque. Sed dignissim, libero sed faucibus mollis, felis tellus laoreet leo, at pretium nunc libero ut justo. Proin sagittis interdum bibendum. Aenean sit amet neque erat. Nulla quis maximus ligula. Integer egestas feugiat metus, in aliquet sem rutrum consequat.',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '2',
+      message: 'Aliquet bibendum enim facilisis gravida neque. Imperdiet massa tincidunt nunc pulvinar. Scelerisque felis imperdiet proin fermentum leo. Porta lorem mollis aliquam ut porttitor leo. Tellus in hac habitasse platea dictumst. Volutpat sed cras ornare arcu dui vivamus. Sed arcu non odio euismod. Morbi tincidunt ornare massa eget egestas purus viverra accumsan. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Eu facilisis sed odio morbi quis commodo. Diam volutpat commodo sed egestas egestas.',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '3',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '4',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '5',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '6',
+      message: 'Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Amet nulla facilisi morbi tempus iaculis. Dui ut ornare lectus sit amet est. Pharetra vel turpis nunc eget lorem dolor sed viverra. Tristique magna sit amet purus gravida quis blandit turpis cursus. Mauris vitae ultricies leo integer. Mauris nunc congue nisi vitae suscipit tellus mauris. Ultricies lacus sed turpis tincidunt id. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Id diam maecenas ultricies mi eget. Vel risus commodo viverra maecenas accumsan lacus vel. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. At quis risus sed vulputate odio ut. Ac auctor augue mauris augue. Dignissim convallis aenean et tortor at risus. Commodo ullamcorper a lacus vestibulum sed arcu non.',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '7',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '8',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '8',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '8',
+      message: 'This is a message',
+      username: 'Timothy',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    }
+  ],
+  general: [
+    {
+      id: '1',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non molestie urna. Suspendisse dolor lacus, suscipit id malesuada et, congue id sem. Praesent ut sollicitudin nisl. Fusce mollis, urna ut feugiat imperdiet, odio orci mollis nunc, quis imperdiet quam urna vel erat. Nam auctor sed nulla vel venenatis. Nullam ac risus ut est scelerisque molestie. Fusce sed euismod lorem, sed tempus neque. Sed dignissim, libero sed faucibus mollis, felis tellus laoreet leo, at pretium nunc libero ut justo. Proin sagittis interdum bibendum. Aenean sit amet neque erat. Nulla quis maximus ligula. Integer egestas feugiat metus, in aliquet sem rutrum consequat.',
+      username: 'Bob',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '1',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non molestie urna. Suspendisse dolor lacus, suscipit id malesuada et, congue id sem. Praesent ut sollicitudin nisl. Fusce mollis, urna ut feugiat imperdiet, odio orci mollis nunc, quis imperdiet quam urna vel erat. Nam auctor sed nulla vel venenatis. Nullam ac risus ut est scelerisque molestie. Fusce sed euismod lorem, sed tempus neque. Sed dignissim, libero sed faucibus mollis, felis tellus laoreet leo, at pretium nunc libero ut justo. Proin sagittis interdum bibendum. Aenean sit amet neque erat. Nulla quis maximus ligula. Integer egestas feugiat metus, in aliquet sem rutrum consequat.',
+      username: 'Dave',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+    {
+      id: '1',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non molestie urna. Suspendisse dolor lacus, suscipit id malesuada et, congue id sem. Praesent ut sollicitudin nisl. Fusce mollis, urna ut feugiat imperdiet, odio orci mollis nunc, quis imperdiet quam urna vel erat. Nam auctor sed nulla vel venenatis. Nullam ac risus ut est scelerisque molestie. Fusce sed euismod lorem, sed tempus neque. Sed dignissim, libero sed faucibus mollis, felis tellus laoreet leo, at pretium nunc libero ut justo. Proin sagittis interdum bibendum. Aenean sit amet neque erat. Nulla quis maximus ligula. Integer egestas feugiat metus, in aliquet sem rutrum consequat.',
+      username: 'Sarah',
+      profilePic: 'https://ca.slack-edge.com/TS07UEB19-USHJ29D2R-g05947c7423d-512'
+    },
+  ]
+}
+
+/* exports.getMessages = (callback) => {
   setTimeout(() => {
     callback(null, messages)
   }, 0)
 }
+ */
