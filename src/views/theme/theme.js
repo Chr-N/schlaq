a9eaf0bea66378cdeb3a6f3cc832a452645e90a5
Chris <cng177@my.bcit.ca>
2020-03-28 22:20:35 -0700

    Update theme

--- src/views/theme/theme.js
+++ src/views/theme/theme.js
@@ -3,12 +3,19 @@ export default {
   bgSidebar: 'rgb(25,23,29)',
   bgHeader: 'rgb(18,16,22)',
   bgInput: 'rgb(34,37,41)',
+
   txt: 'rgb(209,210,211)',
+  txtBright: 'rgb(255,255,255)',
   txtSec: 'rgb(171,171,173)',
   txtDim: 'rgb(145,145,147)',
+
   online: 'rgb(43,172,118)',
   highlight: 'rgb(17,100,163)',
   link: 'rgb(17,100,163)',
-  outline: 'rgb(51,51,51)',
-  hover: 'rgb(51,51,51)'
+  hover: 'rgba(255,255,255,0.1)',
+  focusShadow: '0 0 0 2px rgba(255,255,255,0.2)',
+  boxShadow: `0 0 0 1px rgba(255,255,255,0.1)`,
+  boxShadowInput: `0 0 0 1px rgba(255,255,255,0.2)`,
+  focusShadowInput: `0 0 0 1px rgba(255,255,255,0.5)`,
+  overlay: 'rgba(255,255,255,0.3)'
 }
