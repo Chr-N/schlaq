ada766d1e1e6a20f74b8c5d185dcd433db52a32c
Chris <cng177@my.bcit.ca>
2020-03-28 22:20:10 -0700

    Add dynamic props + routes to server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const reactViews = require('express-react-views')
-const { port, viewsDir, engineOpts, props } = require('./globals')
-const { messages } = require('./db/db')
+const { port, viewsDir, engineOpts } = require('./globals')
+const { title } = require('./globals').props
+const { starred, channels, dms, messages } = require('./db/db')
 
 const server = express()
 
@@ -10,7 +11,24 @@ server.set( 'views', viewsDir )
 server.set( 'view engine', 'jsx' )
 server.engine( 'jsx', reactViews.createEngine( engineOpts ) )
 
-server.get( '/', (req,res) => res.render( 'App', {...props, name: 'bob'} ) )
-server.get( '/messages', (req,res) => res.render( 'App', {...props, messages } ) )
+server.get( '/', (req,res) => res.render( 'App', {
+  title,
+  starred,
+  channels,
+  dms,
+  messages,
+  scope: 'Bob'
+}))
+
+server.get( '/:scope', (req,res) => res.render( 'App', {
+  title,
+  starred,
+  channels,
+  dms,
+  messages,
+  scope: req.params.scope,
+  scopeStarred: starred.map( ({ starred }) => starred ).includes( req.params.scope )
+}))
+
 
 server.listen( port, () => console.log( `\nServer is live at http://localhost:${port}` ) )
