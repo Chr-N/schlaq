4b4c51b6bb3eb7af7470ae38b9c8b72dad2db04c
Chris <cng177@my.bcit.ca>
2020-03-30 13:24:33 -0700

    Add client-side primusLib + script

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -8,8 +8,8 @@ import { css } from '@emotion/core'
 
 const styles = css`background-color:black;width:100vw;height:100vh;display:grid;grid-template-columns:260px 1fr;grid-template-rows:35px 1fr;grid-template-areas:'header header' 'sidebar main';`
 
-export default ({ title, user, workspaceScope, workspaces, scope, scopeStarred, starred, channels, dms, messages }) => (
-  <Layout title={title}>
+export default ({ title, user, workspaceScope, workspaces, scope, scopeStarred, starred, channels, dms, messages, primusLib }) => (
+  <Layout title={title} primusLib={primusLib}>
     <div css={styles}>
       <Header />
       <Sidebar
@@ -27,8 +27,5 @@ export default ({ title, user, workspaceScope, workspaces, scope, scopeStarred, 
         messages={messages}
       />
     </div>
-    <script dangerouslySetInnerHTML={{ __html:`
-      console.log('hi')
-    `}} />
   </Layout>
 )
--- src/views/layouts/Layout.jsx
+++ src/views/layouts/Layout.jsx
@@ -4,12 +4,14 @@ import theme from '../styles/theme'
 
 const scrollbar = css`overflow:overlay;&::-webkit-scrollbar{width:0.6rem;height:0.6rem;};&::-webkit-scrollbar-thumb{background-color:${theme.overlay};border-radius:0.3rem;}&::-webkit-scrollbar-corner{background-color:${theme.bg};}`
 
-export default ({ title, children }) => (
+export default ({ title, primusLib, children }) => (
   <html>
     <head>
       <title>{title}</title>
       <link rel='stylesheet' href='../tw/base.min.css' />
       <link rel='shortcut icon' href='../favicon.svg' type='image/svg+xml' />
+      <script async dangerouslySetInnerHTML={{ __html: primusLib }} />
+      <script async defer src='../script.js' />
     </head>
     <body css={[scrollbar,css`color:${theme.txt};background-color:${theme.bg};`]}>
       {children}
