b1a099b956bc4196545d56db76d818bfc966d20e
Chris <cng177@my.bcit.ca>
2020-03-29 17:59:40 -0700

    Scaffold user + workspace scope handling

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const reactViews = require('express-react-views')
 const { port, viewsDir, engineOpts } = require('./globals')
 const { title } = require('./globals').props
-const { starred, channels, dms, messages } = require('./db/db')
+const { users, starred, channels, dms, messages } = require('./db/db')
 const starredArr = starred.map( ({ starred }) => starred )
 
 const server = express()
@@ -12,16 +12,22 @@ server.set( 'views', viewsDir )
 server.set( 'view engine', 'jsx' )
 server.engine( 'jsx', reactViews.createEngine( engineOpts ) )
 
-server.get( '/', (req,res) => res.redirect('/general') )
+server.get( '/', (req,res) => res.redirect('/FSWD/general') )
 
-server.get( '/:scope', (req,res) => res.render( 'App', {
+server.get( '/:workspace/:scope', (req,res) => {
+  console.log(req.params)
+
+  res.render( 'App', {
   title,
+  user: 'Timothy',
+  workspaceScope: req.params.workspace,
+  workspaces: users['Timothy'],
   starred,
   channels,
   dms,
   messages,
   scope: req.params.scope,
   scopeStarred: starredArr.includes( req.params.scope )
-}))
+})})
 
 server.listen( port, () => console.log( `\nServer is live at http://localhost:${port}` ) )
