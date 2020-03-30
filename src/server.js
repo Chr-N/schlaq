e42bdca1b53fc90dbb83001444427948a9cf79cd
Chris <cng177@my.bcit.ca>
2020-03-30 13:58:51 -0700

    Merge app.js with server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const reactViews = require('express-react-views')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
 const http = require('http')
 const Primus = require('primus')
 const Terser = require('terser')
@@ -7,6 +10,9 @@ const Terser = require('terser')
 const { port, primusPort, viewsDir, engineOpts } = require('./globals')
 const expressRoutes = require('./routes/express')
 const primusRoutes = require('./routes/primus')
+const loginRoute = require('./routes/auth/login-endpoint')
+const signUpRoute = require('./routes/auth/signup-endpoint')
+const dbRoute = require('./routes/sql-endpoints')
 
 const server = express()
 const httpServer = http.createServer(server)
@@ -18,9 +24,17 @@ const primus = new Primus( httpServer, {
 server.locals.primusLib = Terser.minify( primus.library() ).code
 
 server.use( express.static( 'public' ) )
+server.use( express.json() )
+server.use( express.urlencoded({ extended: true }) )
+server.use( cookieParser() )
+
 server.set( 'views', viewsDir )
 server.set( 'view engine', 'jsx' )
 server.engine( 'jsx', reactViews.createEngine( engineOpts ) )
+
+server.use( '/login', loginRoute )
+server.use( '/signup', signUpRoute )
+server.use( '/db', dbRoute )
 
 server.get( '/', expressRoutes.index )
 server.get( '/:workspace/:scope', expressRoutes.workspaceScope )
