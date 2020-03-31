f70882f21dc154b61a7fc3496b2b2bb4423899b8
Chris <cng177@my.bcit.ca>
2020-03-31 16:15:43 -0700

    Update script.js for messaging

--- public/script.js
+++ public/script.js
@@ -1,14 +1,25 @@
-const primus = Primus.connect( 'http://localhost:5001', { manual: false } )
-primus.on( 'open' , () => console.log('connected') )
-
-primus.on( 'data', (data) => {
-  console.log( 'server:', data )
-})
+const primus = Primus.connect( `http://${dbHostAddress}:5001`, { manual: false } )
+primus.on( 'open' , () => console.log( 'connected' ) )
 
 document.querySelector('.message-form').onsubmit = (e) => {
   e.preventDefault()
 
   const message = e.target.children[1].value
   e.target.children[1].value = null
-  primus.write({ message })
+  primus.write( message )
 }
+
+const getMessageElem = (profilePic, username, message) => {
+  const messageTemplate = document.querySelector('.message-elem').cloneNode(true)
+
+  messageTemplate.children[1].src = profilePic
+  messageTemplate.children[3].innerText = username
+  messageTemplate.children[5].innerText = message
+
+  return messageTemplate
+}
+
+const messagesScope = document.querySelector('.messages-scope')
+primus.on( 'data', ({ profilePic, username, message }) => {
+  messagesScope.prepend( getMessageElem( profilePic, username, message ) )
+})
